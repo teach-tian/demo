023b15d695305eb13a43610f0a5905eadb5d2dd6
teacher <282799389@qq.com>
2020-12-14 15:49:17 +0800

    版本1.00

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    写自己的组件
+
+    。。。。。。。。。
+
+    123456
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
